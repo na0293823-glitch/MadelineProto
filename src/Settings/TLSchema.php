af8473bf47cada9981bfb6fe3f97d77d28c65a4f
Alexander Pankratov <s.pankratoff@gmail.com>
2025-06-04 22:04:50 +0200

    Revert "Revert"
    This reverts commit 40492645f45c1fe92230aa75749b2e9b0282cc90.

--- src/Settings/TLSchema.php
+++ src/Settings/TLSchema.php
@@ -65,20 +65,12 @@ final class TLSchema extends SettingsAbstract
      */
     public function __wakeup(): void
     {
-        $exists = false;
-        try {
-            $exists = file_exists($this->APISchema);
-        } catch (Throwable) {
-        }
-        // Scheme was upgraded or path has changed
-        if (!$exists) {
-            $new = new self;
-            $this->setAPISchema($new->getAPISchema());
-            $this->setMTProtoSchema($new->getMTProtoSchema());
-            $this->setSecretSchema($new->getSecretSchema());
-            $this->setLayer($new->getLayer());
-            $this->wasUpgraded = true;
-        }
+        $new = new self;
+        $this->setAPISchema($new->getAPISchema());
+        $this->setMTProtoSchema($new->getMTProtoSchema());
+        $this->setSecretSchema($new->getSecretSchema());
+        $this->setLayer($new->getLayer());
+        $this->wasUpgraded = true;
     }
     /**
      * Returns whether the TL parser should re-parse the TL schemes.

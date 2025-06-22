c6df785cd0fd1912558bf2222dd91ca74fe4b94b
Daniil Gentili <daniil@daniil.it>
2025-06-22 18:00:40 +0200

    Fix

--- tests/test.sh
+++ tests/test.sh
@@ -4,7 +4,15 @@ set -ex
 
 export COMPOSER_PROCESS_TIMEOUT=100000
 
-apk add procps git unzip github-cli openssh
+apt-get install procps git unzip gh openssh-client
+sudo mkdir -p -m 755 /etc/apt/keyrings
+out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg
+cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null
+chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg
+mkdir -p -m 755 /etc/apt/sources.list.d
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+apt update
+apt install gh -y
 
 cd /tmp
 

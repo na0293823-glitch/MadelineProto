3a8662b5285b85b70736eba63ae3e56fd0eac8ad
Daniil Gentili <daniil@daniil.it>
2025-06-22 18:03:43 +0200

    Fix

--- tests/makephar.sh
+++ tests/makephar.sh
@@ -8,12 +8,12 @@ php composer-setup.php
 php -r "unlink('composer-setup.php');"
 mv composer.phar /usr/local/bin/composer
 
-sudo mkdir -p -m 755 /etc/apt/keyrings
+mkdir -p -m 755 /etc/apt/keyrings
 out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg
-cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null
+cat $out | tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null
 chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg
 mkdir -p -m 755 /etc/apt/sources.list.d
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 apt update
 apt-get install procps git unzip gh openssh-client -y
 
